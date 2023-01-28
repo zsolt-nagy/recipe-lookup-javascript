ba5b65f5589ded57c951c0476e125c77b52f3011
Zsolt Nagy <info@zsoltnagy.eu>
2023-01-28 21:18:16 +0100

    Submit event handling

--- food-lookup.js
+++ food-lookup.js
@@ -1,3 +1,14 @@
+const form = document.querySelector('.js-food-form');
+const foodContainer = document.querySelector('.js-food-container');
+
 // https://www.themealdb.com/api/json/v1/1/search.php?s=Pie
 
 console.log('JavaScript is running');
+
+function callback(event) {
+    event.preventDefault(); // first thing in the form
+    // event.stopPropagation(); // in case of other events, read event bubbling
+    alert('form got submitted');
+}
+
+form.addEventListener('submit', callback);
